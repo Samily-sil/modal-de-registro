5f3b2ac64dc155504ac5c08b6ff90a54daaa1002
Samily <samily.sss2005@gmail.com>
2025-03-24 19:44:25 -0300

    finalizado

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,20 @@
-/* Desenvolva seu código aqui... */
+function modalFunction () {
+    const link = document.querySelector('.form__register-link');
+    const modalContainer = document.querySelector('.cadastro');
+    const closeButton = document.querySelector('.fechar__modal');
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        modalContainer.showModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+        modalContainer.close();
+    });
+
+    modalContainer.addEventListener("click", (event) => {
+        if(event.target === modalContainer) {
+            modalContainer.close()
+        }
+    });
+}
+modalFunction()
